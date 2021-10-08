87aab56119b3c2ad4e488cee559aca1ddf11aa9b
Find-Astronaut <liang031103@163.com>
2021-10-08 21:24:47 +0800

    NEW

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "alicloud_vswitch" "main" {
   # 交换机地址块
   cidr_block        = "30.1.1.0/24"
   # 可用区
-  availability_zone = "cn-shenzhen-c"
+  zone_id = "cn-shenzhen-a"
   # 资源依赖,会优先创建该依赖资源
   depends_on = [alicloud_vpc.main]
 }
