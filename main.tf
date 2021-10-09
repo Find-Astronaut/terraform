8271e196ba089a50883f73ede2ee342b49ef7bb8
Find-Astronaut <liang031130@163.com>
2021-10-09 20:35:04 +0800

    New1

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 provider "alicloud" {
   configuration_source = "terraform-provider-alicloud/examples/vpc"
 }
-
+// 创建VPC
 resource "alicloud_vpc" "vpc" {
   vpc_name = "VPC10"
   cidr_block = "30.0.0.0/12"
 }
-
+//创建 vSwitch 定义地址 地区
 resource "alicloud_vswitch" "vsw" {
   zone_id = "cn-guangzhou-a"
   cidr_block = "30.1.0.0/21"
   vpc_id = alicloud_vpc.vpc.id
 }
-
+// 默认的安全组
 resource "alicloud_security_group" "default" {
   name = "default"
   vpc_id = alicloud_vpc.vpc.id
 }
-
+//定义策略
 resource "alicloud_security_group_rule" "allow_all_tcp" {
   ip_protocol = "tcp"
   security_group_id = alicloud_security_group.default.id
